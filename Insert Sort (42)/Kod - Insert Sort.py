368c899f797015f3fc3e6d11e26ec673bcc29c57
Vojtěch Schreiner <schreinervojtech@gmail.com>
2022-02-08 11:12:32 +0100

    Vyřešen nekonečný cyklus, Sort stále nefunkční

--- Insert Sort (42)/Kod - Insert Sort.py
+++ Insert Sort (42)/Kod - Insert Sort.py
@@ -5,12 +5,11 @@ print(len(vstup))
 def InsertSort(vstup):
     for i in range(len(vstup)):
         a=vstup[i]
-        while i>0:
-            o=i-1
-            while a<vstup[o]:
-                vstup[o+1]=vstup[o]
-                o=o-1
-        i=i+1    
+        #while i>0:
+        o=i-1
+        while i>0 and a<vstup[o]:
+            vstup[o+1]=vstup[o]
+            o=o-1  
     return()
 
 InsertSort(vstup)
