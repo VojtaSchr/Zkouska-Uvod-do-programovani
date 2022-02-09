1001d635fa38f30284b6a381306893664b7c8d33
Vojtěch Schreiner <schreinervojtech@gmail.com>
2022-02-09 12:57:09 +0100

    Šifra: Velká písmena a speciální znaky

--- Caesarova šifra (109)/Kod - Caesarovova šifra.py
+++ Caesarova šifra (109)/Kod - Caesarovova šifra.py
@@ -1,11 +1,19 @@
-vstup = "ahoj"
+vstup = "ahojz KYč :)"
 vystup = "Výstup: "
 posun = 3
+
+print(ord("a"))
+print(ord("A"))
 
 def sifra(input, exitus, trabea):
     for i in range(len(input)):
         ave = input[i]
-        exitus += chr((ord(ave)+ trabea - 97) %26 + 97)
+        if 97<=ord(ave)<=122:
+            exitus += chr((ord(ave)+ trabea - 97) %26 + 97)
+        elif 65<=ord(ave)<=90:
+            exitus += chr((ord(ave)+ trabea - 65) %26 + 65)
+        else:
+            exitus += ave
     return exitus
 
 #sifra(vstup, vystup, posun)
