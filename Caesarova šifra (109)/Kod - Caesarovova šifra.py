a842e2012e5782ad06d55ffce0f370900e6fae07
Vojtěch Schreiner <schreinervojtech@gmail.com>
2022-02-09 13:04:40 +0100

    Přidáno dešifrování

--- Caesarova šifra (109)/Kod - Caesarovova šifra.py
+++ Caesarova šifra (109)/Kod - Caesarovova šifra.py
@@ -1,11 +1,12 @@
 vstup = "ahojz KYč :)"
-vystup = "Výstup: "
+vystup_sif = "Výstup šifrování: "
+vystup_desif = "Výstup dešifrování: "
 posun = 3
 
-print(ord("a"))
-print(ord("A"))
+#print(ord("a"))
+#print(ord("A"))
 
-def sifra(input, exitus, trabea):
+def sifrator(input, exitus, trabea):
     for i in range(len(input)):
         ave = input[i]
         if 97<=ord(ave)<=122:
@@ -16,5 +17,17 @@ def sifra(input, exitus, trabea):
             exitus += ave
     return exitus
 
+def desifrator(input, exitus, trabea):
+    for i in range(len(input)):
+        ave = input[i]
+        if 97<=ord(ave)<=122:
+            exitus += chr((ord(ave)- trabea - 97) %26 + 97)
+        elif 65<=ord(ave)<=90:
+            exitus += chr((ord(ave)- trabea - 65) %26 + 65)
+        else:
+            exitus += ave
+    return exitus
+
 #sifra(vstup, vystup, posun)
-print(sifra(vstup, vystup, posun))
+print(sifrator(vstup, vystup_sif, posun))
+print(desifrator(vstup, vystup_desif, posun))
