f7bd7c5cfe9051a4aa25412eae78ac38bc434a4f
Vojtěch Schreiner <schreinervojtech@gmail.com>
2022-02-11 22:38:44 +0100

    Final

--- Insert Sort (42)/Kod - Insert Sort.py
+++ Insert Sort (42)/Kod - Insert Sort.py
@@ -1,15 +1,6 @@
-import csv
-#seznam = [4, 8, 2, 0.5, 99, -3, -1, 8]
-seznam =[]
+list_in = [4, 8, 2, 0.5, 99, -3, -1, 8]
 
-input_number=float(input("Input first number: "))
-seznam = input_number
-while input_number!="":
-    input_number =float(input("Input one number: "))
-    while input_number!="":
-        seznam += input_number
-
-print("Length of imput list is " + str(len(seznam)) + " numbers.")
+print("Length of imput list is " + str(len(list_in)) + " numbers.")
 
 def InsertSort(data):
     #Sorting funcion
@@ -24,12 +15,8 @@ def InsertSort(data):
         data[o+1]=a  
     return()
 
-#with open('Vstup_Sort.csv', newline='') as csvin:
-#    csv.register_dialect("dialect1", delimiter=";")
-#    reader = csv.reader(csvin, dialect="dialect1")
-#    seznam = list(reader)
-InsertSort(seznam)
+InsertSort(list_in)
 print("Sorted list:", end=" ")
 #Print sorted output
-for i in range(len(seznam)):
-    print(seznam[i], end=", ")
+for i in range(len(list_in)):
+    print(list_in[i], end=", ")
