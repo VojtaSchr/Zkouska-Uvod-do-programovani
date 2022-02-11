e5b71e515b7e24a9e0ce9e7218ee1c321ab57f83
Vojtěch Schreiner <schreinervojtech@gmail.com>
2022-02-11 20:41:04 +0100

    Pokusy s novými vstupy

--- Insert Sort (42)/Kod - Insert Sort.py
+++ Insert Sort (42)/Kod - Insert Sort.py
@@ -1,19 +1,35 @@
-seznam = [4, 8, 2, 0.5, 99, -3, -1, 8]
+import csv
+#seznam = [4, 8, 2, 0.5, 99, -3, -1, 8]
+seznam =[]
 
-print("Délka vstupního seznamu je " + str(len(seznam)) + " hodnot.")
+input_number=float(input("Input first number: "))
+seznam = input_number
+while input_number!="":
+    input_number =float(input("Input one number: "))
+    while input_number!="":
+        seznam += input_number
 
-def InsertSort(vstup):
-    for i in range(len(vstup)):
-        a=vstup[i]
-        #while i>0:
+print("Length of imput list is " + str(len(seznam)) + " numbers.")
+
+def InsertSort(data):
+    #Sorting funcion
+    #Function is repeated according to the number of characters in the input
+    for i in range(len(data)):
+        a=data[i]
         o=i-1
-        while o>=0 and a<vstup[o]:
-            vstup[o+1]=vstup[o]
+        #When the sorted value is greater than the previous one, it moves to the left 
+        while o>=0 and a<data[o]:
+            data[o+1]=data[o]
             o=o-1
-        vstup[o+1]=a  
+        data[o+1]=a  
     return()
 
+#with open('Vstup_Sort.csv', newline='') as csvin:
+#    csv.register_dialect("dialect1", delimiter=";")
+#    reader = csv.reader(csvin, dialect="dialect1")
+#    seznam = list(reader)
 InsertSort(seznam)
-print("Setřídění seznam čísel:", end=" ")
+print("Sorted list:", end=" ")
+#Print sorted output
 for i in range(len(seznam)):
     print(seznam[i], end=", ")
